4cc84407c245c46ba5ae42168c822cc75864ea57
InsertSweat <769356353@qq.com>
2016-12-04 11:48:00 +0800

    init project

---
+++ src/components/Main.js
@@ -0,0 +1,27 @@
+require('normalize.css/normalize.css');
+require('styles/App.scss');
+
+
+import React from 'react';
+let yeomanImage = require('../images/yeoman.png');
+
+ 
+
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <section  className="stage">
+        <section className="img-sec">
+        </section>
+        <nav className="controller-nav">
+        </nav>
+      </section>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
